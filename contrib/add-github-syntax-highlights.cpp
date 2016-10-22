5907a8225c72e1cf10c67902c898694a30719f2f
galik <galik@users.noreply.github.com>
2016-10-22 18:17:39 +0100

    added args to select input between files and stdio

--- contrib/add-github-syntax-highlights.cpp
+++ contrib/add-github-syntax-highlights.cpp
@@ -6,52 +6,143 @@
  */
 
 #include <string>
+#include <cstring>
+#include <fstream>
+#include <sstream>
 #include <iostream>
+
+#define STD_OUT(m) do{std::cout << m << '\n';}while(0)
+#define STD_ERR(m) do{std::cerr << m << '\n';}while(0)
+#define throw_runtime(m) do{std::ostringstream o;o<<m;throw std::runtime_error(o.str());}while(0)
+#define throw_errno(m) throw_runtime(m << ": " << std::strerror(errno))
+
+int usage(std::string prog, int error_code = EXIT_SUCCESS);
+
+void process_markdown(std::istream& is, std::ostream& os);
 
 int main(int, char** argv)
 {
-	std::istream& is = std::cin;
-	std::ostream& os = std::cout;
-
 	try
 	{
-		bool in_dent = false;
-		auto eol = "";
+		std::string i_file_name;
+		std::string o_file_name;
 
-		for(std::string line; std::getline(is, line);)
+		for(auto arg = argv + 1; *arg; ++arg)
 		{
-			if(!in_dent && !line.find("    "))
+			if(!std::strcmp(*arg, "-h") || !std::strcmp(*arg, "--help"))
+				return usage(argv[0]);
+			else
 			{
-				in_dent = true;
-				os << eol << "```cpp";
-				eol = "\n";
+				// not a recognized option, treat it like a file name
+				if(i_file_name.empty())
+					i_file_name = *arg;
+				else if(o_file_name.empty())
+					o_file_name = *arg;
+				else // there should only be a maximum of 2 file names
+				{
+					STD_ERR("Unknown parameter: " << *arg);
+					return usage(EXIT_FAILURE);
+				}
 			}
-			else if(in_dent && !line.empty() && line[0] != ' ')
-			{
-				in_dent = false;
-				os << eol << "```";
-				eol = "\n";
-			}
-
-			if(in_dent && line.size() > 3)
-				line.erase(0, 4);
-
-			os << eol << line;
-			eol = "\n";
 		}
+
+		// if an input filename was given try to open a file for it
+		std::ifstream ifs;
+		if(!i_file_name.empty())
+			ifs.open(i_file_name);
+
+		// a filename was given and it failed to open
+		if(!ifs)
+			throw_errno("error: " << i_file_name);
+
+		// if an output filename was given try to open a file for it
+		std::ofstream ofs;
+		if(!o_file_name.empty())
+			ifs.open(o_file_name);
+
+		// a filename was given and it failed to open
+		if(!ofs)
+			throw_errno("error: " << o_file_name);
+
+		// only use the files if they are open, otherwise use the standard streams
+		process_markdown(ifs.is_open() ? ifs : std::cin, ofs.is_open() ? ofs : std::cout);
 	}
 	catch(std::exception const& e)
 	{
-		std::cerr << e.what() << '\n';
+		STD_ERR(e.what());
 		return EXIT_FAILURE;
 	}
 	catch(...)
 	{
-		std::cerr << "Unknown exception." << '\n';
+		STD_ERR("Unknown exception.");
 		return EXIT_FAILURE;
 	}
 
 	return EXIT_SUCCESS;
 }
 
+int usage(std::string prog, int error_code)
+{
+	if(auto pos = prog.rfind('/') + 1)
+		prog.erase(0, pos);
+
+	STD_OUT("");
+	STD_OUT("Usage: " << prog << " [OPTIONS] [<input> [<output>]]");
+	STD_OUT("");
+	STD_OUT("                If no input file <input> is specified");
+	STD_OUT("                  the input is taken from the standard input.");
+	STD_OUT("");
+	STD_OUT("                If no output file <output> is specified");
+	STD_OUT("                  the output is sent to the standard output.");
+	STD_OUT("");
+	STD_OUT("OPTIONS");
+	STD_OUT("");
+	STD_OUT(" -h|--help    - Print this help massage.");
+	STD_OUT("");
+
+	return error_code;
+}
+
+class line_ender
+{
+public:
+	line_ender() {}
+	line_ender(line_ender const&) = delete;
+	line_ender& operator=(line_ender const&) = delete;
+
+private:
+	char const* eol = "";
+
+	friend std::ostream& operator<<(std::ostream& o, line_ender& le)
+	{
+		o << le.eol;
+		le.eol = "\n";
+		return o;
+	}
+};
+
+void process_markdown(std::istream& is, std::ostream& os)
+{
+	line_ender eol;
+	bool indented = false;
+
+	for(std::string line; std::getline(is, line);)
+	{
+		if(!indented && !line.find("    "))
+		{
+			indented = true;
+			os << eol << "```cpp";
+		}
+		else if(indented && !line.empty() && line[0] != ' ')
+		{
+			indented = false;
+			os << eol << "```";
+		}
+
+		if(indented && line.size() > 3)
+			line.erase(0, 4);
+
+		os << eol << line;
+	}
+}
 
